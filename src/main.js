3c8168eb391ae0d15bfa7b299ff3712a783934af
amir <amirtaki623@gmail.com>
2025-07-03 23:11:16 +0330

    load show part 5 at project

--- src/main.js
+++ src/main.js
@@ -12,5 +12,14 @@ const loadShow = () => {
         items[i].style.filter = "blur(5px)"
         items[i].style.opacity = stt > 2 ? 0 : .6
     }   
+    stt = 0
+    for(let i = active - 1; i >= 0; i--){
+        stt++;
+        items[i].style.transform = `translateX(${-120*stt}px) scale(${1 - .2*stt})  perspective(16px)  rotateY(-1deg)`;
+        items[i].style.zIndex = -stt;
+        items[i].style.filter = "blur(5px)"
+        items[i].style.opacity = stt > 2 ? 0 : .6
+
+    }
 }
 loadShow()
