81be6a5a5614149a8a88a6e7b90725c8d811fb48
amir <amirtaki623@gmail.com>
2025-07-03 23:01:54 +0330

    load show at project

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
-let items = document.querySelectorAll(".items")
+let items = document.querySelectorAll(".item")
 let next = document.getElementById("next")
 let prev = document.getElementById("prev")
+
+let active = 3;
+const loadShow = () => {
+    let stt = 0;
+    for(let i = active + 1; i < items.length; i++){
+        console.log(i)
+        stt++;
+        console.log(items[i])
+        items[i].style.transform = `translateX(${120*stt})`;
+    }
+}
+loadShow()
