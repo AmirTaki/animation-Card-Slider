c975bb87ecf5326aee0d43f442aec59fdd454e76
amir <amirtaki623@gmail.com>
2025-07-03 23:13:12 +0330

    load show part 6  at project

--- src/main.js
+++ src/main.js
@@ -5,6 +5,10 @@ let prev = document.getElementById("prev")
 let active = 3;
 const loadShow = () => {
     let stt = 0;
+    items[active].style.transform = "none"
+    items[active].style.zIndex = 1;
+    items[active].style.filter = "none"
+    items[active].style.opacity = 1
     for(let i = active + 1; i < items.length; i++){
         stt++;
         items[i].style.transform = `translateX(${120*stt}px) scale(${1 - .2*stt})  perspective(16px)  rotateY(-1deg)`;
