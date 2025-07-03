13ee6a99ab6ccef72fe4739bff160742a39ca739
amir <amirtaki623@gmail.com>
2025-07-03 23:07:53 +0330

    load show part 3 at project

--- src/main.js
+++ src/main.js
@@ -7,7 +7,8 @@ const loadShow = () => {
     let stt = 0;
     for(let i = active + 1; i < items.length; i++){
         stt++;
-        items[i].style.transform = `translateX(${120*stt}px) scale(${1 - .2*stt})`;
+        items[i].style.transform = `translateX(${120*stt}px) scale(${1 - .2*stt})  perspective(16px)  rotateY(-1deg)`;
+        items[i].style.zIndex = -stt;
     }
 }
 loadShow()
